7c3c7f840c5c922583431a7eb0a9c01f6ea1eac5
Emil Wallberg <49481622+EmilWallberg@users.noreply.github.com>
2025-07-08 22:19:29 +0000

    Inital commit

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require("wezterm")
+
+local config = {}
+
+config.color_scheme = "ayu"
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 20
+
+config.enable_tab_bar = false
+
+return config
